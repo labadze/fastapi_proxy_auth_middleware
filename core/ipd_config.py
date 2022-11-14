74129b402ab645af8e4b4f93ef3afab61d28502b
decapitator <root@localhost.local>
2022-11-14 15:35:11 +0400

    Remove database

--- core/ipd_config.py
+++ core/ipd_config.py
@@ -1,15 +1,15 @@
 import os
 
 from fastapi import FastAPI
-from fastapi_keycloak import FastAPIKeycloak, OIDCUser, UsernamePassword, HTTPMethod, KeycloakUser, KeycloakGroup
+from fastapi_keycloak import FastAPIKeycloak
 
 app = FastAPI()
 idp = FastAPIKeycloak(
     server_url=os.getenv("KEYCLOAK_SERVER_URL", default="http://localhost:8080/auth"),
     client_id=os.getenv("KEYCLOAK_CLIENT_ID", default="fapy"),
-    client_secret=os.getenv("KEYCLOAK_CLIENT_SECRET", default="XzuBCwsaaszhazCc1GNeUKPgTNc9hZAR"),
-    admin_client_secret=os.getenv("KEYCLOAK_ADMIN_CLIENT_SECRET", default="tja7jaR7dqdHHacrGqY1aGH8FlsoSId4"),
+    client_secret=os.getenv("KEYCLOAK_CLIENT_SECRET", default="17uKutcUraGPnBCkINpa8VmVFrLMsvMy"),
+    admin_client_secret=os.getenv("KEYCLOAK_ADMIN_CLIENT_SECRET", default="3Xr687BkDuDOZlNdEAEOJ96fFCSon9DL"),
     realm=os.getenv("KEYCLOAK_REALM", default="fapy"),
-    callback_uri=os.getenv("BACK_END_URL", default="http://localhost:3000/callback"),
+    callback_uri=os.getenv("KEYCLOAK_CALLBACK_URL", default="http://localhost:3000/callback"),
 )
 idp.add_swagger_config(app)
